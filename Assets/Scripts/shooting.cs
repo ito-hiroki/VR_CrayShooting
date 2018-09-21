5e5b55d1e9935224953f5da568bca29a5f2ec49a
hir0king <i.hiroki0307@icloud.com>
2018-09-21 16:58:51 +0900

    コントローラから弾が出るようにした

---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+    public float speed = 1000;
+
+    void Update()
+    {
+        var trackedObject = GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("トリガーを深く引いた");
+
+            GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+
+            bullet.transform.position = this.transform.position;
+            bullet.transform.rotation = this.transform.rotation;
+            bullet.transform.Rotate(new Vector3(90, 0, 0) , Space.Self);
+
+            Vector3 force;
+            force = this.gameObject.transform.forward * speed;
+            bullet.GetComponent<Rigidbody>().AddForce(force);
+            
+        }
+    }
+}
